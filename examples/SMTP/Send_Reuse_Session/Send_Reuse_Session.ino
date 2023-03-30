d62375864d675a2fc1ddf48203141f5c97139814
unknown <k_suwatchai@hotmail.com>
2023-03-30 10:55:27 +0700

    Update SMTP example.

--- examples/SMTP/Send_Reuse_Session/Send_Reuse_Session.ino
+++ examples/SMTP/Send_Reuse_Session/Send_Reuse_Session.ino
@@ -76,289 +76,162 @@
 
 /* Recipient email address */
 #define RECIPIENT_EMAIL "<recipient email here>"
-#define RECIPIENT_EMAIL2 "<recipient email here>"
-#define RECIPIENT_EMAIL3 "<recipient email here>"
-#define RECIPIENT_EMAIL4 "<recipient email here>"
 
-/* Declare the global used SMTPSession object for SMTP transport */
+
 SMTPSession smtp;
 
-/* Callback function to get the Email sending status */
+Session_Config config;
+
 void smtpCallback(SMTP_Status status);
 
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
 WiFiMulti multi;
 #endif
 
-// For Free Heap checking
 #include "HeapStat.h"
 HeapStat heapInfo;
+
+unsigned long sentMillis = 0;
 
 void setup()
 {
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
 #if defined(ARDUINO_ARCH_SAMD)
-  while (!Serial)
-    ;
-  Serial.println();
-  Serial.println("**** Custom built WiFiNINA firmware need to be installed.****\n");
-  Serial.println("To install firmware, read the instruction here, https://github.com/mobizt/ESP-Mail-Client#install-custom-build-wifinina-firmware");
+    while (!Serial)
+        ;
+    Serial.println();
+    Serial.println("**** Custom built WiFiNINA firmware need to be installed.****\n");
+    Serial.println("To install firmware, read the instruction here, https://github.com/mobizt/ESP-Mail-Client#install-custom-build-wifinina-firmware");
 #endif
 
-  Serial.println();
+    Serial.println();
 
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-  multi.addAP(WIFI_SSID, WIFI_PASSWORD);
-  multi.run();
+    multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+    multi.run();
 #else
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 #endif
 
-  Serial.print("Connecting to Wi-Fi");
-  unsigned long ms = millis();
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
+    Serial.print("Connecting to Wi-Fi");
+    unsigned long ms = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-    if (millis() - ms > 10000)
-      break;
+        if (millis() - ms > 10000)
+            break;
 #endif
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
 
-  /*  Set the network reconnection option */
-  MailClient.networkReconnect(true);
+    MailClient.networkReconnect(true);
 
-  // The WiFi credentials are required for Pico W
-  // due to it does not have reconnect feature.
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-  MailClient.clearAP();
-  MailClient.addAP(WIFI_SSID, WIFI_PASSWORD);
+    MailClient.clearAP();
+    MailClient.addAP(WIFI_SSID, WIFI_PASSWORD);
 #endif
 
-  /** Enable the debug via Serial port
-   * 0 for no debugging
-   * 1 for basic level debugging
-   *
-   * Debug port can be changed via ESP_MAIL_DEFAULT_DEBUG_PORT in ESP_Mail_FS.h
-   */
-  smtp.debug(1);
+    smtp.debug(1);
 
-  /* Set the callback function to get the sending results */
-  smtp.callback(smtpCallback);
+    smtp.callback(smtpCallback);
 
-  /* Declare the Session_Config for user defined session credentials */
-  Session_Config config;
+    config.server.host_name = SMTP_HOST;
+    config.server.port = SMTP_PORT;
+    config.login.email = AUTHOR_EMAIL;
+    config.login.password = AUTHOR_PASSWORD;
 
-  /* Set the session config */
-  config.server.host_name = SMTP_HOST;
-  config.server.port = SMTP_PORT;
-  config.login.email = AUTHOR_EMAIL;
-  config.login.password = AUTHOR_PASSWORD;
+    config.login.user_domain = F("mydomain.net");
 
-  /** Assign your host name or you public IPv4 or IPv6 only
-   * as this is the part of EHLO/HELO command to identify the client system
-   * to prevent connection rejection.
-   * If host name or public IP is not available, ignore this or
-   * use generic host "mydomain.net".
-   *
-   * Assign any text to this option may cause the connection rejection.
-   */
-  config.login.user_domain = F("mydomain.net");
-
-  /* Set the NTP config time */
-  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
-  config.time.gmt_offset = 3;
-  config.time.day_light_offset = 0;
-
-  /* Declare the message class */
-  SMTP_Message message;
-
-  /* Set the message headers */
-  message.sender.name = F("ESP Mail");
-  message.sender.email = AUTHOR_EMAIL;
-  message.subject = F("First Email with session reusage");
-  message.addRecipient(F("Admin1"), RECIPIENT_EMAIL);
-  message.addRecipient(F("Admin2"), RECIPIENT_EMAIL2);
-  message.addCc(RECIPIENT_EMAIL3);
-  message.addBcc(RECIPIENT_EMAIL4);
-
-  message.html.content = F("<p>This is the <span style=\"color:#ff0000;\">first message</span>.</p>");
-
-  /** The HTML text message character set e.g.
-   * us-ascii
-   * utf-8
-   * utf-7
-   * The default value is utf-8
-   */
-  message.html.charSet = F("utf-8");
-
-  /** The content transfer encoding e.g.
-   * enc_7bit or "7bit" (not encoded)
-   * enc_qp or "quoted-printable" (encoded)
-   * enc_base64 or "base64" (encoded)
-   * enc_binary or "binary" (not encoded)
-   * enc_8bit or "8bit" (not encoded)
-   * The default value is "7bit"
-   */
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_qp;
-
-  /** The option to add soft line break to to the message for
-   * the long text message > 78 characters (rfc 3676)
-   * Some Servers may not compliant with the standard.
-   */
-  message.text.flowed = true;
-
-  message.text.content = F("This is the first message");
-  message.text.charSet = F("us-ascii");
-
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_base64;
-
-  /** The message priority
-   * esp_mail_smtp_priority_high or 1
-   * esp_mail_smtp_priority_normal or 3
-   * esp_mail_smtp_priority_low or 5
-   * The default value is esp_mail_smtp_priority_low
-   */
-  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
-
-  /** The Delivery Status Notifications e.g.
-   * esp_mail_smtp_notify_never
-   * esp_mail_smtp_notify_success
-   * esp_mail_smtp_notify_failure
-   * esp_mail_smtp_notify_delay
-   * The default value is esp_mail_smtp_notify_never
-   */
-  // message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
-
-  /* Set the custom message header */
-  message.addHeader(F("Message-ID: <Admin1@gmail.com>"));
-
-  Serial.println();
-  Serial.println("Sending first Email...");
-
-  /* Connect to the server */
-  if (!smtp.connect(&config))
-    return;
-
-  if (!smtp.isLoggedIn())
-  {
-    Serial.println("\nNot yet logged in.");
-  }
-  else
-  {
-    if (smtp.isAuthenticated())
-      Serial.println("\nSuccessfully logged in.");
-    else
-      Serial.println("\nConnected with no Auth.");
-  }
-
-  /** Start sending the first Email and keep open the session
-   * The third parameter is for close the config.
-   */
-  if (!MailClient.sendMail(&smtp, &message, false))
-    Serial.println("Error sending Email, " + smtp.errorReason());
-
-  // If the session closed by server or internet interruption,
-  // the function sendMail will return false with error.
-  // You may need to manually close the session to re-authenticate periodically.
-
-  /* To close session manually */
-  // smtp.closeSession();
-
-  /* To clear all message data */
-  // message.clear();
-
-  /** Clear primary recipients, Cc recipients, Bcc recipients, custom headers
-   * attachments and inline images
-   */
-  message.clearRecipients();
-  message.clearCc();
-  message.clearBcc();
-  // message.clearAttachments();
-  // message.clearInlineimages();
-
-  message.subject = F("Second Email with session reusage");
-
-  message.html.content = F("<p>This is the <span style=\"color:#ff0000;\">second message</span>.</p>");
-  message.html.charSet = F("us-ascii");
-
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
-
-  message.text.content = F("This is the second message");
-  message.text.charSet = F("UTF-8");
-  message.text.transfer_encoding = Content_Transfer_Encoding::enc_qp;
-
-  /* Set the custom message header */
-  message.addHeader(F("Message-ID: <Admin3@gmail.com>"));
-
-  Serial.println();
-  Serial.println("Sending second Email...");
-
-  /* Start sending the second mail and close the session */
-  if (!MailClient.sendMail(&smtp, &message))
-    Serial.println("Error sending Email, " + smtp.errorReason());
-
-  // to clear sending result log
-  // smtp.sendingResult.clear();
-
-  ESP_MAIL_PRINTF("Free Heap: %d\n", MailClient.getFreeHeap());
+    config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+    config.time.gmt_offset = 3;
+    config.time.day_light_offset = 0;
 }
 
 void loop()
 {
+    if (millis() - sentMillis > 3 * 60 *1000 || sentMillis == 0)
+    {
+        sentMillis = millis();
+
+        SMTP_Message message;
+
+        message.sender.name = F("ESP Mail");
+        message.sender.email = AUTHOR_EMAIL;
+        message.subject = F("Send Email with session reusage");
+        message.addRecipient(F("user"), RECIPIENT_EMAIL);
+
+        message.html.content = F("<p>This is the HTML message.</p>");
+        message.text.content = F("This is the text message");
+
+        Serial.println();
+        Serial.println("Sending Email...");
+
+        if (!smtp.isLoggedIn())
+        {
+            if (!smtp.connect(&config))
+            {
+                Serial.println("\nError, SMTP server connection failed.");
+                goto exit;
+            }
+
+            if (!smtp.isLoggedIn())
+            {
+                Serial.println("\nError, Not yet logged in.");
+                goto exit;
+            }
+            else
+            {
+                if (smtp.isAuthenticated())
+                    Serial.println("\nSuccessfully logged in.");
+                else
+                    Serial.println("\nConnected with no Auth.");
+            }
+        }
+
+        if (!MailClient.sendMail(&smtp, &message, false))
+            Serial.println("Email sending error, " + smtp.errorReason());
+
+    exit:
+
+        heapInfo.collect();
+        heapInfo.print();
+    }
 }
 
 /* Callback function to get the Email sending status */
 void smtpCallback(SMTP_Status status)
 {
-  /* Print the current status */
-  Serial.println(status.info());
 
-  /* Print the sending result */
-  if (status.success())
-  {
-    // ESP_MAIL_PRINTF used in the examples is for format printing via debug Serial port
-    // that works for all supported Arduino platform SDKs e.g. AVR, SAMD, ESP32 and ESP8266.
-    // In ESP8266 and ESP32, you can use Serial.printf directly.
+    Serial.println(status.info());
 
-    Serial.println("----------------");
-    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
-    ESP_MAIL_PRINTF("Message sent failed: %d\n", status.failedCount());
-    Serial.println("----------------\n");
-
-    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
+    if (status.success())
     {
-      /* Get the result item */
-      SMTP_Result result = smtp.sendingResult.getItem(i);
 
-      // In case, ESP32, ESP8266 and SAMD device, the timestamp get from result.timestamp should be valid if
-      // your device time was synched with NTP server.
-      // Other devices may show invalid timestamp as the device time was not set i.e. it will show Jan 1, 1970.
-      // You can call smtp.setSystemTime(xxx) to set device time manually. Where xxx is timestamp (seconds since Jan 1, 1970)
-      time_t ts = (time_t)result.timestamp;
+        Serial.println("----------------");
+        ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+        ESP_MAIL_PRINTF("Message sent failed: %d\n", status.failedCount());
+        Serial.println("----------------\n");
 
-      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
-      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
-      ESP_MAIL_PRINTF("Date/Time: %s\n", asctime(localtime(&ts)));
-      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
-      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+        for (size_t i = 0; i < smtp.sendingResult.size(); i++)
+        {
+            SMTP_Result result = smtp.sendingResult.getItem(i);
+            time_t ts = (time_t)result.timestamp;
+
+            ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+            ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+            ESP_MAIL_PRINTF("Date/Time: %s\n", asctime(localtime(&ts)));
+            ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+            ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+        }
+        Serial.println("----------------\n");
+
+        smtp.sendingResult.clear();
     }
-    Serial.println("----------------\n");
-
-    // You need to clear sending result as the memory usage will grow up.
-    smtp.sendingResult.clear();
-
-    // Collect memory info
-    heapInfo.collect();
-
-    // Print memory info
-    heapInfo.print();
-  }
 }
