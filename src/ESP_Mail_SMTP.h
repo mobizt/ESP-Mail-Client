31c1816af2048a7c1d348a42ee4968ed09ff175e
unknown <k_suwatchai@hotmail.com>
2024-07-20 09:17:56 +0700

    Fix SMTP message sending result issue #349

--- src/ESP_Mail_SMTP.h
+++ src/ESP_Mail_SMTP.h
@@ -5,7 +5,7 @@
 /**
  * Mail Client Arduino Library for Espressif's ESP32 and ESP8266, Raspberry Pi RP2040 Pico, and SAMD21 with u-blox NINA-W102 WiFi/Bluetooth module
  *
- * Created July 19, 2024
+ * Created July 20, 2024
  *
  * This library allows Espressif's ESP32, ESP8266, SAMD and RP2040 Pico devices to send and read Email through the SMTP and IMAP servers.
  *
